5abba95968758100cc72dd10d5de8dfc47821142
ozzerio <74795656+ozzerio@users.noreply.github.com>
2024-06-21 15:09:09 +0000

    aggiunta configurazione doppler +

--- container/main.tf
+++ container/main.tf
@@ -1,5 +1,10 @@
 terraform {
   required_providers {
+
+    doppler = {
+      source  = "DopplerHQ/doppler"
+      version = "1.8.0"
+    }
     google = {
       source  = "hashicorp/google"
       version = "4.51.0"
@@ -7,14 +12,89 @@ terraform {
   }
 }
 
+
+
+
 variable "GOOGLE_CLOUD_PROJECT_ID" {
+  type = string
+}
+
+variable "DOPPLER_TOKEN" {
   type = string
 }
 
 provider "google" {
   project = var.GOOGLE_CLOUD_PROJECT_ID
-  
+
 }
+
+
+
+provider "doppler" { // Doppler secret manager for PostgreSQL (read/write access)
+  doppler_token = var.DOPPLER_TOKEN
+  alias         = "my_project"
+}
+
+
+data "doppler_secrets" "my_project" {
+  provider = doppler.my_project
+
+}
+
+/*output "pippo" {
+  value = nonsensitive(data.doppler_secrets.my_project.map.SECRET1)
+}*/
+
+
+
+
+resource "google_sql_database_instance" "masterdb" {
+  provider = google
+
+  region  = "us-central1"
+  project = var.GOOGLE_CLOUD_PROJECT_ID
+
+  name                = "db1"
+  database_version    = "POSTGRES_14"
+  deletion_protection = false       // WARNING!!!
+  settings {                        // Define Cloud SQL settings
+    tier            = "db-g1-small" // Tier of the Machine (# of CPUs, RAM and Disk size)
+    disk_autoresize = true          // Auto-resize the Disk space if not enough for current data
+
+  }
+}
+
+
+resource "google_sql_user" "db1user1" {
+  provider = google
+
+  instance = google_sql_database_instance.masterdb.name
+
+  // To grant readonly permissions to an user run the following script:
+  // https://github.com/hashicorp/terraform-provider-google/issues/10438
+  name     = nonsensitive(data.doppler_secrets.my_project.map.MASTER_DB_USER1)
+  password = data.doppler_secrets.my_project.map.MASTER_DB_USER1_PSW
+}
+
+
+
+
+resource "doppler_secret" "db1_host" {
+  provider = doppler.my_project
+
+  // Reserved secrets, see: https://docs.doppler.com/docs/secrets#reserved-secrets
+  config  = nonsensitive(data.doppler_secrets.my_project.map.DOPPLER_CONFIG)
+  project = nonsensitive(data.doppler_secrets.my_project.map.DOPPLER_PROJECT)
+
+  // Sets the 'name' variable to the output 'value' 
+  name       = "MASTER_DB_HOST"
+  value      = google_sql_database_instance.masterdb.public_ip_address
+  visibility = "unmasked"
+}
+
+
+
+
 
 resource "google_cloud_run_service" "middleware_cloudrun_instance" {
   provider = google
@@ -28,8 +108,9 @@ resource "google_cloud_run_service" "middleware_cloudrun_instance" {
   // For reference, see: https://cloud.google.com/run/docs/reference/rpc/google.cloud.run.v1#revisiontemplate
   metadata {
     // See: https://cloud.google.com/sdk/gcloud/reference/run/deploy#--ingress
-    annotations = { "run.googleapis.com/ingress" = "all" }
+    annotations = { "run.googleapis.com/ingress" = "all", "run.googleapis.com/operation-id" = "83eb5dc9-b709-43e5-8b53-0758838ad192" }
   }
+
 
   template { // Describes the data our CloudRun revision should have when created
     spec {
@@ -52,7 +133,7 @@ resource "google_cloud_run_service" "middleware_cloudrun_instance" {
     percent         = 100
     latest_revision = true
   }
- 
+
 
 }
 
@@ -66,9 +147,9 @@ data "google_iam_policy" "noauth" {
 }
 
 resource "google_cloud_run_service_iam_policy" "noauth" {
-  location    = google_cloud_run_service.middleware_cloudrun_instance.location
-  project     = google_cloud_run_service.middleware_cloudrun_instance.project
-  service     = google_cloud_run_service.middleware_cloudrun_instance.name
+  location = google_cloud_run_service.middleware_cloudrun_instance.location
+  project  = google_cloud_run_service.middleware_cloudrun_instance.project
+  service  = google_cloud_run_service.middleware_cloudrun_instance.name
 
   policy_data = data.google_iam_policy.noauth.policy_data
 }
